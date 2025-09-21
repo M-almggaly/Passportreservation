feb32717c417677ac8af2f02c12f96c4a28e61e9
M-ALmggaly <mohammed@localhost.localdomain>
2025-09-21 14:31:04 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,243 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>SIFAR</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+<!--  <!-- Favicons--> -->
+<!--  <link href="assets/img/jar-with-fresh-honey (1).jpg" rel="icon">-->
+<!--  <link href="assets/img/jar-with-fresh-honey (1).jpg" rel="apple-touch-icon"> -->
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+  <link href="assets/css/style.css" rel="stylesheet">
+  <!-- Link Swiper's CSS -->
+  <link rel="stylesheet" href="swiper-8.4.4/package/swiper-bundle.min.css" />
+  <script src="swiper-8.4.4/package/swiper-bundle.min.js"></script>
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <header id="header">
+    <div class="container d-flex align-items-center justify-content-between" dir="rtl">
+
+      <div class="logo">
+        <h1><a dir="rtl" href="index.html"><span>.</span>Sifar</a></h1>
+        <!-- Uncomment below if you prefer to use an image logo -->
+        <!-- <a href="index.php"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+      </div>
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a class="nav-link scrollto active" href="#hero">الرئيسية</a></li>
+          <li><a class="nav-link scrollto" href="#about">عنا</a></li>
+          <li><a class="nav-link scrollto" href="#services">خدماتنا</a></li>
+          <li><a class="getstarted scrollto" href="#about">ابدا معنا</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+    </div>
+  </header><!-- End Header -->
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero" class="hero-header">
+
+    <div class="container">
+      <div class="row d-flex align-items-center">
+      <div dir="rtl" class=" col-lg-6 py-5 py-lg-0 order-2 order-lg-1" data-aos="fade-right">
+        <h1 style="font-family: 'Almarai', sans-serif;">اطلب جواز سفرك بسهولة عبر منصتنا</h1>
+        <h2 style="font-family: 'Almarai', sans-serif;">خدمة إلكترونية موثوقة وسريعة من مصلحة الهجرة والجوازات</h2>
+        <a href="" class="btn-get-started scrollto">اطلب جوازك</a>
+      </div>
+      <div class="col-lg-6 order-1 order-lg-2  my-5" data-aos="fade-left">
+        <img src="assets/img/جواز-سفر-اليمن-scaled-1-780x470-removebg-preview.png"  alt="">
+      </div>
+    </div>
+    </div>
+
+  </section><!-- End Hero -->
+
+  <main id="main">
+
+      <!-- ======= About Section ======= -->
+      <section id="about" class="about section-bg mohammed">
+          <div class="container">
+
+              <div dir="rtl" class="row gy-4">
+                  <div class="image col-xl-5"></div>
+                  <div class="col-xl-7">
+                      <div class="content d-flex flex-column justify-content-center ps-0 ps-xl-4">
+                          <h3 style="font-family: 'Almarai', sans-serif;" data-aos="fade-in" data-aos-delay="100">من نحن؟</h3>
+                          <p data-aos="fade-up">
+                              نحن منصة إلكترونية تابعة لمصلحة الهجرة والجوازات، نقدم لك خدمة طلب واستخراج جواز السفر بسهولة وسرعة ومن أي مكان.
+                          </p>
+                          <div class="row gy-4 mt-3">
+                              <div class="col-md-6" data-aos="fade-up" data-aos-delay="100">
+                                  <h4><a style="font-family: 'Almarai', sans-serif;" href="#">ماذا نقدم لك؟</a></h4>
+                                  <p>
+                                      - تقديم طلبات استخراج وتجديد الجوازات إلكترونيًا بكل بساطة.<br>
+                                      - متابعة حالة طلبك خطوة بخطوة حتى استلام الجواز.<br>
+                                      - دعم فني متاح لمساعدتك طوال عملية التقديم.<br>
+                                      - توفير بيئة آمنة وسهلة الاستخدام لتوفير وقتك وجهدك.
+                                  </p>
+                              </div>
+                          </div>
+                      </div><!-- End .content-->
+                  </div>
+              </div>
+
+          </div>
+      </section><!-- End About Section -->
+
+
+      <!-- ======= Services Section ======= -->
+      <section id="services" class="services section-bg">
+          <div class="container">
+
+              <div class="section-title">
+                  <h2 style="font-family: 'Almarai', sans-serif;" data-aos="fade-in">خدماتنا</h2>
+                  <p dir="rtl" data-aos="fade-in">
+                      نوفر لك مجموعة من الخدمات الإلكترونية لتسهيل عملية طلب واستخراج جواز السفر من مصلحة الهجرة والجوازات بسرعة وأمان.
+                  </p>
+              </div>
+
+              <div class="row">
+                  <div class="col-md-6 d-flex" data-aos="fade-right">
+                      <div class="card">
+                          <div class="card-img">
+                              <img src="assets/img/Submitanapplication.jpg" alt="تقديم طلب إلكتروني">
+                          </div>
+                          <div class="card-body">
+                              <h5 style="font-family: 'Almarai', sans-serif;" class="card-title"><a href="#">تقديم طلب الجواز إلكترونيًا</a></h5>
+                              <p dir="rtl" class="card-text">
+                                  يمكنك تقديم طلبك للحصول على جواز السفر أو تجديده عبر الإنترنت بسهولة دون الحاجة لزيارة المكاتب الحكومية إلا عند الاستلام.
+                              </p>
+                          </div>
+                      </div>
+                  </div>
+
+                  <div class="col-md-6 d-flex" data-aos="fade-left">
+                      <div class="card">
+                          <div class="card-img">
+                              <img src="assets/img/2769565.jpg" alt="تتبع الطلب">
+                          </div>
+                          <div class="card-body">
+                              <h5 style="font-family: 'Almarai', sans-serif;" class="card-title"><a href="#">تتبع حالة الطلب</a></h5>
+                              <p dir="rtl" class="card-text">
+                                  وفّر وقتك وجهدك بمتابعة حالة طلبك لحظة بلحظة من خلال لوحة التحكم الخاصة بك حتى اكتمال استخراج الجواز.
+                              </p>
+                          </div>
+                      </div>
+                  </div>
+
+                  <div class="col-md-6 d-flex" data-aos="fade-right">
+                      <div class="card">
+                          <div class="card-img">
+                              <img src="assets/img/460823-PG5VFF-381.jpg" alt="خدمة الدعم الفني">
+                          </div>
+                          <div class="card-body">
+                              <h5 style="font-family: 'Almarai', sans-serif;" class="card-title"><a href="#">دعم فني واستشارات</a></h5>
+                              <p dir="rtl" class="card-text">
+                                  فريق الدعم جاهز للإجابة عن استفساراتك ومساعدتك في جميع خطوات تقديم الطلب حتى استلام الجواز.
+                              </p>
+                          </div>
+                      </div>
+                  </div>
+
+                  <div class="col-md-6 d-flex" data-aos="fade-left">
+                      <div class="card">
+                          <div class="card-img">
+                              <img src="assets/img/2697668.jpg" alt="خدمة الاستلام السريع">
+                          </div>
+                          <div class="card-body">
+                              <h5 style="font-family: 'Almarai', sans-serif;" class="card-title"><a href="#">استلام سريع وآمن</a></h5>
+                              <p dir="rtl" class="card-text">
+                                  بمجرد تجهيز جوازك، يمكنك استلامه بسرعة وأمان من أقرب فرع أو من خلال خدمة التوصيل حسب المتاح في منطقتك.
+                              </p>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+
+          </div>
+      </section><!-- End Services Section -->
+
+
+      <!-- ======= Footer ======= -->
+  <footer id="footer">
+
+    <div class="footer-top">
+
+      <div class="container">
+
+        <div class="row  justify-content-center">
+            <div class="col-lg-6">
+                <h3>SIFAR</h3>
+                <p dir="rtl" style="font-family: 'Almarai', sans-serif;" >
+                    نعمل جاهدين لتسهيل حصولك على جواز السفر بسرعة وأمان. يسعدنا تواصلك معنا أو اشتراكك في تنبيهات الموقع
+                    لمتابعة أحدث التحديثات والخدمات. آراؤك واقتراحاتك تهمنا لتحسين تجربتك دائمًا.
+                </p>
+            </div>
+
+        </div>
+
+        <div class="row footer-newsletter justify-content-center">
+          <div class="col-lg-6">
+            <form action="" method="post">
+              <input class="" type="email" name="email" placeholder="Enter your Email"><input type="submit" value="Subscribe">
+            </form>
+          </div>
+        </div>
+
+        <div class="social-links">
+          <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+          <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+          <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+          <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
+          <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+        </div>
+
+      </div>
+    </div>
+
+    <div class="container footer-bottom clearfix">
+      <div class="copyright">
+        &copy; Copyright <strong><span>sifar</span></strong>. All Rights Reserved
+      </div>
+      <div class="credits">
+
+      </div>
+    </div>
+  </footer><!-- End Footer -->
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
